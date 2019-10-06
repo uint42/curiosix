7c1765a5ae4b7f96e21680ddbe6a9e760c8f380e
Joh4nnesB <52888466+Joh4nnesB@users.noreply.github.com>
2019-10-06 13:53:48 +0200

    Translated actions menu/about screen

--- src/scripting/gui/ActionMenu.ts
+++ src/scripting/gui/ActionMenu.ts
@@ -3,6 +3,7 @@ import Interpreter from '../language/interpreter/Interpreter'
 import CodeEditor from './CodeEditor'
 import Swal from 'sweetalert2'
 import ThemeChanger from '../../utils/ThemeChanger'
+import { t } from '../../utils/i18n'
 
 class ActionMenu {
   private allowResume: boolean = false
@@ -22,8 +23,8 @@ class ActionMenu {
     document.getElementById('restart').onclick = _ => {
       if (!curiosix.world || !curiosix.world.rover || !curiosix.fileManager.scriptFileTypeManager.fileSelector.currentFileName) {
         Swal.fire({
-          title: 'Skript kann nicht ausgeführt werden',
-          text: 'Damit ein Skript ausgeführt werden kann, muss eine Skript und eine Welt geladen in der sich ein Rover befindet',
+          title: t('scripting.execution_not_possible.title'),
+          text: t('scripting.execution_not_possible.text'),
           type: 'error'
         })
         return
@@ -34,21 +35,21 @@ class ActionMenu {
     document.getElementById('about').onclick = _ => {
       const webpackEnv = require('../../utils/WebpackEnv')
       Swal.fire({
-        title: 'Über CuriosiX',
+        title: t('about.title'),
         html: `
           <img src="${require('../../../assets/images/logo.svg')}" height="100px"><br>
-          <div>Version: <b>${webpackEnv.buildInfo.version}</b> (${webpackEnv.development ? 'Entwicklerversion' : 'Nutzerversion'})</div>
-          <div>Build-Informationen: <b>${webpackEnv.buildInfo.os}</b></div>
-          <div>Build-Datum: <b>${webpackEnv.buildInfo.date}</b></div>
+          <div>${t('about.version')}: <b>${webpackEnv.buildInfo.version}</b> (${webpackEnv.development ? t('about.version_type.development') : t('about.version_type.production')})</div>
+          <div>${t('about.build_info')}: <b>${webpackEnv.buildInfo.os}</b></div>
+          <div>${t('about.build_date')}: <b>${webpackEnv.buildInfo.date}</b></div>
           <br>
           <div>&copy; Johannes Bauer 2019</div>
           <br>
-          <button id="osl" class="btn">Open Source Lizenzen</button>
+          <button id="osl" class="btn">${t('about.open_source_licences')}</button>
         `,
         onOpen: _ => {
           document.getElementById('osl').onclick = _ => {
             Swal.fire({
-              title: 'Open Source Lizenen',
+              title: t('about.open_source_licences'),
               html: `
               <div>three.js (by mrdoob and other three.js authors): <a href="https://raw.githubusercontent.com/mrdoob/three.js/dev/LICENSE">MIT</a></div>
               <div>codemirror (by Marijn Haverbeke and others): <a href="https://raw.githubusercontent.com/codemirror/CodeMirror/master/LICENSE">MIT</a></div>
@@ -59,7 +60,8 @@ class ActionMenu {
               <br>
               <div>Roboto Font (by Christian Robertson / Google): <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache License 2.0</a></div>
               <div>Material Icons (by Google): <a href="https://raw.githubusercontent.com/google/material-design-icons/master/LICENSE">Apache License 2.0</a></div>
-              `
+              `,
+              width: '50%'
             })
           }
         },
